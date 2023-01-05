a01b0c91dd10c5e03540c8ba8d4ffcbaadf39f45
Max <maxkomas@yandex.ru>
2023-01-06 00:31:04 +0300

    js8f

---
+++ JS8f/2/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Задание 2
+let str2 = 'aaa bbb ccc';
+str = str2.replace(/bbb/,'');
+console.log(str2); 
+
+
+
+
+
+
+
---
+++ JS8f/4/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Задание 4
+let str = 'какой-то текст';
+let n = str.slice(0, 5);
+let result = n +'...';
+console.log(result); 
+
+
+
+
+
+
